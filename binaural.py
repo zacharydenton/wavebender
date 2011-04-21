af860d06d19070927b8ba75d3aa5a658c16793e1
Zach Denton <zacharydenton@gmail.com>
2011-04-20 21:47:38 -0400

    organized files for use as a library
    * added binaural beat generator

---
+++ binaural.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from wavegen import *
+
+channels = ((sine_wave(440.0, amplitude=0.1),),
+            (sine_wave(445.0, amplitude=0.1),))
+
+samples = compute_samples(channels, 44100 * 1)
+write_wavefile('5hz.wav', samples)
--- wavegen.py
+++ wavegen.py
@@ -17,8 +17,16 @@ def sine_wave(frequency=440.0, framerate=44100, amplitude=0.5):
     '''
     if amplitude > 1.0: amplitude = 1.0
     if amplitude < 0.0: amplitude = 0.0
-    return (float(amplitude) * math.sin(2.0*math.pi*float(frequency)*(float(i)/float(framerate))) \
-            for i in count(0))
+    return (float(amplitude) * math.sin(2.0*math.pi*float(frequency)*(float(i)/float(framerate))) for i in count(0))
+
+def square_wave(frequency=440.0, framerate=44100, amplitude=0.5):
+    for s in sine_wave(frequency, framerate, amplitude):
+        if s > 0:
+            yield amplitude
+        elif s < 0:
+            yield -amplitude
+        else:
+            yield 0.0
 
 def white_noise(amplitude=0.5):
     '''
@@ -26,7 +34,16 @@ def white_noise(amplitude=0.5):
     '''
     return (float(amplitude) * random.uniform(-1, 1) for i in count(0))
 
-def write_wavefile(filename, samples, nchannels, sampwidth, framerate):
+def compute_samples(channels, nsamples):
+    '''
+    create a generator which computes the samples.
+
+    essentially it creates a sequence of the sum of each function 
+    at each sample in the file for each channel.
+    '''
+    return islice(izip(*(imap(sum, izip(*channel)) for channel in channels)), nsamples)
+
+def write_wavefile(filename, samples, nchannels=2, sampwidth=2, framerate=44100):
     "Write samples to a wavefile."
     w = wave.open(filename, 'w')
     w.setparams((nchannels, sampwidth, framerate, 0, 'NONE', 'not compressed'))
@@ -47,7 +64,7 @@ def main():
     parser.add_argument('-c', '--channels', help="Number of channels to produce", default=2, type=int)
     parser.add_argument('-b', '--bits', help="Number of bits in each sample", choices=(16,), default=16, type=int)
     parser.add_argument('-r', '--rate', help="Sample rate in Hz", default=44100, type=int)
-    parser.add_argument('-d', '--duration', help="Duration of the wave in seconds.", default=60, type=int)
+    parser.add_argument('-t', '--time', help="Duration of the wave in seconds.", default=60, type=int)
     parser.add_argument('-a', '--amplitude', help="Amplitude of the wave on a scale of 0.0-1.0.", default=0.5, type=float)
     parser.add_argument('-f', '--frequency', help="Frequency of the wave in Hz", default=440.0, type=float)
     parser.add_argument('filename', help="The file to generate.")
@@ -56,9 +73,8 @@ def main():
     # each channel is defined by infinite functions which are added to produce a sample.
     channels = ((sine_wave(args.frequency, args.rate, args.amplitude), white_noise(0.1)) for i in range(args.channels))
 
-    # now we create a generator which computes the samples.
-    # essentially it creates a sequence of the sum of each function at each sample in the file for each channel.
-    samples = islice(izip(*(imap(sum, izip(*channel)) for channel in channels)), args.rate * args.duration)
+    # convert the channel functions into waveforms
+    samples = compute_samples(channels, args.rate * args.time)
 
     # write the samples to a file
     write_wavefile(args.filename, samples, args.channels, args.bits / 8, args.rate)
