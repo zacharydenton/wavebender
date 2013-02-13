23431d9847aa1f7991d03a74f94d4610ed00c8d1
Zach Denton <zacharydenton@gmail.com>
2013-02-12 22:42:21 -0500

    Fix sine wave generation.

--- wavebender/__init__.py
+++ wavebender/__init__.py
@@ -34,11 +34,11 @@ def sine_wave(frequency=440.0, framerate=44100, amplitude=0.5):
     '''
     Generate a sine wave at a given frequency of infinite length.
     '''
-    period = int(framerate / frequency)
     if amplitude > 1.0: amplitude = 1.0
     if amplitude < 0.0: amplitude = 0.0
-    lookup_table = [float(amplitude) * math.sin(2.0*math.pi*float(frequency)*(float(i%period)/float(framerate))) for i in xrange(period)]
-    return (lookup_table[i%period] for i in count(0))
+    for i in count(0):
+        sine = math.sin(2.0 * math.pi * float(frequency) * (float(i) / float(framerate)))
+        yield float(amplitude) * sine
 
 def square_wave(frequency=440.0, framerate=44100, amplitude=0.5):
     for s in sine_wave(frequency, framerate, amplitude):
